5bca703cff615890ccbeab9e7989757ce7b5a21b
anna.msklv <anna.msklv@yandex.ru>
2022-10-09 22:42:48 +0300

    unfinished

---
+++ Task3_AverageOfColumn/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindAverageOfColumn(int[,] matrix)  //метод должен возвращать массив из значений для каждого столбца
+{
+    int sumOfColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int count = 0;
+            if (j == count)
+            {
+                sumOfColumn += matrix[i, j];
+                count++;
+            }
+        }
+    }
+    return sumOfColumn;
+    // double average = sumOfColumn / matrix.GetLength(0);
+    return average;
+}
+
+int n = GetNumber("Введите число m");
+int m = GetNumber("Введите число n");
+int[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine("Матрица:");
+PrintMatrix(matrix);
+// double averageOfColumnElements = FindAverageOfColumn(matrix);
+// Console.WriteLine($"Среднее арифметическое каждого столбца:")
